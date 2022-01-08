a2e1ed11ec1c10aeedb28dd6ebb8ccd174ff7919
JCruzMi <47216265+JCruzMi@users.noreply.github.com>
2022-01-08 13:03:22 -0500

    glowup github api do it

--- src/components/pruebas/GithubSearch.vue
+++ src/components/pruebas/GithubSearch.vue
@@ -3,34 +3,106 @@
         <div class="search inner-shadow">
            <div class="input">
                 <i class="fas fa-search"></i>
-               <input class="searc" v-model="user" placeholder="User name">
+                <input class="searc" type="text" v-model="user" placeholder="User name">
            </div>
         <button class="btn-1 outer-shadow" @click="getDataUser">Search</button>
         </div>
         <div class="info outer-shadow">
-            <img :src="dataUser.avatar_url">
-            <h2>{{dataUser.login}}</h2>
-            <span>{{dataUser.created_at.substring(0,10)}}</span>
-            {{dataUser}}
+            <div class="flex">
+                
+                <img :src="dataUser.avatar_url">
+                
+                <div class="data">
+                    <div class="name">
+                        <h2>{{dataUser.name}}</h2>
+                        <div>{{dataUser.created_at.substring(0,10).split("-").join(" ")}}</div>
+                    </div>
+                    <a :href="dataUser.html_url"><i class="fab fa-github"></i>{{dataUser.name}}</a>
+                    <p class="bio">{{dataUser.bio}}</p>
 
+                    <div class="info name inner-shadow">
+                        <div class="text">
+                            <span>repos</span>
+                            <span>{{dataUser.public_repos}}</span>
+                        </div>
+                        <div class="text">
+                            <span>followers</span>
+                            <span>{{dataUser.followers}}</span>
+                        </div>
+                        <div class="text">
+                            <span>following</span>
+                            <span>{{dataUser.following}}</span>
+                        </div>
+                    </div>
+                    <div class="name">
+                        <span><i class="fas fa-map-marker"></i> {{ dataUser.location}}</span>
+                        <span><i class="fas fa-building"></i> {{ dataUser.company}}</span>
+                    </div>
+                    <div class="name">
+                        <a target="_blank" :href="'https://twitter.com/'+dataUser.twitter_username" v-if="dataUser.twitter_username!='Not Avaliable'">
+                        <span ><i class="fab fa-twitter"></i> {{ dataUser.twitter_username}}</span>
+                        </a>
+                        <span v-if="dataUser.twitter_username=='Not Avaliable'"><i class="fab fa-twitter"></i> {{ dataUser.twitter_username}}</span>
+                        <span><i class="fas fa-envelope"></i> {{ dataUser.email}}</span>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
         </div>
     </div>
 </template>
 
 <script setup>
 
-    import { ref } from 'vue'
+    import { ref, onMounted } from 'vue'
 
-
-    const user = ref("")
+    const user = ref("JCruzMi")
 
     const dataUser = ref([])
 
+    const info = {
+        name: "",
+        avatar_url: "This profile has no avatar",
+        created_at: "",
+        html_url: "",
+        bio: "This profile has no bio",
+        public_repos: "",
+        followers: "",
+        following: "",
+        location: "Not Avaliable",
+        company: "Not Avaliable",
+        twitter_username: "Not Avaliable",
+        email: "Not Avaliable"
+    }
+
+    onMounted(() => {
+        getDataUser()
+    })
+ 
     const getDataUser = () =>{
         user.value = user.value.split(" ").join("")
+        if(user.value.length == 0){
+            user.value = "JCruzMi"
+        }
         fetch("https://api.github.com/users/"+user.value).then((res) => res.json())
-        .then((data) => dataUser.value = data)
+        .then((data) => {
+            let refData = {}
+            for(var key in info){
+                if(data[key]!=null){
+                    refData[key]=data[key]
+                    
+                } else {
+                    refData[key]=info[key]
+                }
+            }
+
+            dataUser.value = refData
+        })
     }
+
+    
 
 </script>
 
@@ -51,13 +123,17 @@
 }
 
 
-.searc {
+
+input{
     margin-left: 1rem;
-    width: 300px;
     padding: 1rem;
     border-radius: 10px;
     background-color: transparent;
     border: 0 solid;
+    display: inline-flex;
+    min-width: auto;
+    max-width: auto;
+
 }
 
 .input{
@@ -75,14 +151,91 @@ input{
     font-size: 18px;
 }
 
+.bio{
+    margin-top:1rem;
+    color: var(--text-salmon-700);
+}
+
 .btn-1{
     border-radius: 10px;
+}
+
+.data{
+    width: 100%;
 }
 
 .info{
     margin-top: 1rem;
     padding: 1rem;
     border-radius: 10px;
+}
+
+.info img{
+    width: 160px;
+    height: 160px;
+    border-radius: 50%;
+    margin-bottom: 1rem;
+}
+
+.flex{
+    display: flex;
+    align-items: stretch;
+    gap: 1rem;
+}
+
+.name:first-child{
+    color : var(--text-black-900);
+}
+
+.name{
+    margin-top: 1rem;
+    display: flex;
+    justify-content: space-between;
+    color: var(--text-black-600);
+}
+
+.text span{
+    display: block;
+    color : var(--text-black-900);
+}
+
+i{
+    margin-right: .5rem;
+}
+
+@media (max-width: 560px) {
+    .flex{
+        display: block;
+        align-items: center;
+        justify-content: center;
+    }
+}
+
+@media (max-width: 450px) {
+    .input i{
+        display:None;
+    }
+    .searc{
+        margin-left: 0;
+        width: 100%;
+    }
+}
+
+@media (max-width: 370px) {
+    .name{
+        display: block;
+    }
+    span{
+        display: block;
+    }
+    .data{
+        text-align: center;
+    }
+    .info img{
+        margin-left: 25%;
+    }   
 
 }
+
+
 </style>>
